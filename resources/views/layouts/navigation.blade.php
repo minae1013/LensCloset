8693d5bfc9ad97390727cb3fed3cbc5d9c2c40b0
minae <minae19981013@gmail.com>
2024-12-25 21:43:21 +0900

    ヘッダーのリンクを削除

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -96,11 +96,6 @@
     <!-- Responsive Navigation Menu -->
     @if(Auth::check())
         <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-            <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link :href="route('mylens')" :active="request()->routeIs('mylens')">
-                    {{ __('MyLens') }}
-                </x-responsive-nav-link>
-            </div>
 
             <!-- Responsive Settings Options -->
             <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
