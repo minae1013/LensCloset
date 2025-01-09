526ca9bc06a8e71b1687a729c3dd5c6199e7838d
minae <minae19981013@gmail.com>
2025-01-10 01:06:28 +0900

    項目調整

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -27,12 +27,12 @@ class PostRequest extends FormRequest
             'color' => 'required|string|max:100',
             'lens_diameter' =>'required|numeric',
             'colored_diameter' =>'required|numeric',
-            'lifespan' =>'required|numeric',
+            'lifespan' =>'required',
             'price' =>'required|numeric',
             'image_path' => 'file|image|nullable|mimes:jpg,jpeg,png|max:1024',
-            'rating' =>'required|numeric',
+            'rating' =>'required',
             'comment' =>'required|string|max:500',
-            'repeat' =>'required|numeric',
+            'repeat' =>'required',
         ];
     }
 
@@ -50,18 +50,15 @@ class PostRequest extends FormRequest
             'colored_diameter.required' => '着色直径は必須です。',
             'colored_diameter.numeric' => '着色直径は数値で入力してください。',
             'lifespan.required' => '使用期間は必須です。',
-            'lifespan.numeric' => '使用期間は数値で入力してください。',
             'price.required' => '価格は必須です。',
             'price.numeric' => '価格は数値で入力してください。',
             'image_path.image' => '指定されたファイルが画像形式ではありません。',
             'image_path.mimes' => '画像の拡張子が違います。拡張子はjpg、jpeg、pngのいずれかに設定してください。',
             'image_path.max' => '画像のサイズは1MB以下にしてください。',
             'rating.required' => '評価は必須です。',
-            'rating.numeric' => '評価は数値で入力してください。',
             'comment.required' => 'コメントは必須です。',
             'comment.max' => 'コメントは500文字以内で入力してください。',
             'repeat.required' => 'リピートは必須です。',
-            'repeat.numeric' => 'リピートは数値で入力してください。',
         ];
     }
 }
